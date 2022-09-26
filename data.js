2df5c4e2417bd47d8f42330cbabfcb53e75252e7
Udeh Ifeanyi <ifescoguy@gmail.com>
2022-09-26 21:28:30 +0100

    update

--- data.js
+++ data.js
@@ -22,31 +22,31 @@ const data = [
         bio: "How you doin?",
         hasBeenSwiped: false,
         hasBeenLiked: false
+    },
+    {
+        name: "Jack",
+        avatar: "images/dog-jack.jpg",
+        age: 22,
+        bio: "I like flying",
+        hasBeenSwiped: false,
+        hasBeenLiked: false
+    },
+    {
+        name: "Billy",
+        avatar: "images/dog-billy.jpg",
+        age: 35,
+        bio: "Looks like you don't want me",
+        hasBeenSwiped: false,
+        hasBeenLiked: false
+    },
+    {
+        name: "Bingo",
+        avatar: "images/dog-bingo.jpg",
+        age: 20,
+        bio: "Look into my eye",
+        hasBeenSwiped: false,
+        hasBeenLiked: false
     }
-    // {
-    //     name: "Jack",
-    //     avatar: "images/dog-jack.jpg",
-    //     age: 22,
-    //     bio: "I like flying",
-    //     hasBeenSwiped: false,
-    //     hasBeenLiked: false
-    // },
-    // {
-    //     name: "Billy",
-    //     avatar: "images/dog-billy.jpg",
-    //     age: 35,
-    //     bio: "Looks like you don't want me",
-    //     hasBeenSwiped: false,
-    //     hasBeenLiked: false
-    // },
-    // {
-    //     name: "Bingo",
-    //     avatar: "images/dog-bingo.jpg",
-    //     age: 20,
-    //     bio: "Look into my eye",
-    //     hasBeenSwiped: false,
-    //     hasBeenLiked: false
-    // }
 ]
 
 export default data
